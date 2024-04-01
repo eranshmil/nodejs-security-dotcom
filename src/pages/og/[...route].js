3069a0743d93f75b091b50a0eda1d53e84c80c7d
Liran Tal <liran.tal@gmail.com>
2024-04-01 14:40:57 +0300

    fix: dont overwrite the image key on the frontmatter, caused a bug
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- src/pages/og/[...route].js
+++ src/pages/og/[...route].js
@@ -1,34 +1,38 @@
 import { OGImageRoute } from 'astro-og-canvas';
 
-// Import all pages from the content directory
-const rawPages = await import.meta.glob(`../../../data/blog/**/*.md`, { eager: true });
-
-const pages = Object.entries(rawPages).reduce((acc, [path, page]) => {
-	// @TBD we're consuming the frontmatter like this:
-	// console.log(page.frontmatter);
-
-	//@TBD if no slug then use the file path
-
-	// resolve the `image` frontmatter field to the full path
-	// because right now the convention is magic resolving path like this:
-	// image: '~/assets/images/blog/nodejs-vulnerability-scanner-eol.jpeg',
-	// so we need to resolve it to the full path
-	const image = page.frontmatter.image;
-	if (image && image.startsWith('~/')) {
-		page.frontmatter.image = `./src${image.slice(1)}`;
-	}
-	return { ...acc, [page.frontmatter.slug]: page };
-}, {});
-
 const bgFile = './src/assets/images/og-background.jpg';
+
+async function getPages() {
+	const rawPages = await import.meta.glob('../../../data/blog/**/*.md', { eager: true });
+	const pages = Object.entries(rawPages).reduce((acc, [path, page]) => {
+		// @TBD we're consuming the frontmatter like this:
+		// console.log(page.frontmatter);
+
+		//@TBD if no slug then use the file path
+
+		// resolve the `image` frontmatter field to the full path
+		// because right now the convention is magic resolving path like this:
+		// image: '~/assets/images/blog/nodejs-vulnerability-scanner-eol.jpeg',
+		// so we need to resolve it to the full path
+		const image = page.frontmatter.image;
+
+		if (page.frontmatter.draft) {
+			return acc;
+		}
+
+		if (image && image.startsWith('~/')) {
+			page.frontmatter.imageOgLogo = `./src${image.slice(1)}`;
+		}
+		return { ...acc, [page.frontmatter.slug]: page };
+	}, {});
+	return pages;
+}
 
 export const { getStaticPaths, GET } = OGImageRoute({
 	// The name of your dynamic route segment.
 	// In this case it’s `route`, because the file is named `[...route].ts`.
 	param: 'route',
-
-	// A collection of pages to generate images for.
-	pages,
+	pages: await getPages(),
 
 	// For each page, this callback will be used to customize the OG image.
 	getImageOptions: async (_, page) => {
@@ -36,7 +40,7 @@ export const { getStaticPaths, GET } = OGImageRoute({
 			title: page.frontmatter.title,
 			// description: page.frontmatter.description,
 			logo: {
-				path: page.frontmatter.image,
+				path: page.frontmatter.imageOgLogo,
 				size: [250],
 			},
 			bgImage: {
